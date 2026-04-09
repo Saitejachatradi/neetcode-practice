35dabe3c1a509580b6de96c1e91285e84b107664
Sai Teja <98050865+Saitejachatradi@users.noreply.github.com>
2026-04-09 14:57:45 -0500

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack<>();
+      Map<Character, Character> map = new HashMap<>();
+      map.put('(', ')');
+      map.put('[', ']');
+      map.put('{', '}');
+      for(int i = 0; i < s.length(); i++){
+         char c = s.charAt(i);
+         if(map.containsKey(c)){
+            stack.push(c);
+            return true;
+         }else {
+            if(stack.isEmpty() || map.get(stack.peek()) !=c){
+               return false;
+            }
+            stack.pop();
+         }
+      }
+      return stack.isEmpty();
+      
+    }
+}
