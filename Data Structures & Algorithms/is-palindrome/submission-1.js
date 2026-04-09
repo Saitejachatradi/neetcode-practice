2811016e66590bae6ecf3043adb3720f125ee0e7
Sai Teja <98050865+Saitejachatradi@users.noreply.github.com>
2026-04-09 14:42:54 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+      let i = 0; 
+      let j = s.length - 1;
+      while(i < j){
+         while(i < j && !/[A-Za-z0-9]/.test(s[i])){
+            i++;
+         }
+         while(i < j && !/[A-Za-z0-9]/.test(s[j])){
+            j--;
+         }
+         if(s[i].toLowerCase() != s[j].toLowerCase()){
+            return false;
+         }
+         i++;
+         j--;
+      }
+      return true;
+    }
+}
