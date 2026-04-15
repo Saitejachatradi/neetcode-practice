87b8f530a1b1265f1acdb3d8fd8aa0091716c3e5
Sai Teja <98050865+Saitejachatradi@users.noreply.github.com>
2026-04-15 09:50:27 -0500

    Update: is-palindrome - submission-3

--- Data Structures & Algorithms/is-palindrome/submission-3.js
+++ Data Structures & Algorithms/is-palindrome/submission-3.js
@@ -4,21 +4,19 @@ class Solution {
      * @return {boolean}
      */
     isPalindrome(s) {
-     let i = 0;
-     let j = s.length - 1;
-     while(i < j){
-       while(i < j && !/[A-Za-z0-9]/.test(s.charAt(i))){
-        i++;
-       }
-       while(i < j && !/[A-Za-z0-9]/.test(s.charAt(j))){
-        j--;
-       }
-     if(s[i].toLowerCase() != s[j].toLowerCase()){
-        return false;
-     }
-     i++; 
-     j--;
-     }
-return true;
+        let left = 0;
+        let right = s.length - 1;
+        while(left < right){
+         while(left < right && ![A-za-z0-9].test(s[left])){
+           left++;
+         }
+         while(left < right && ![A-za-z0-9].test(s[right])){
+            right--;
+         }
+         if(s[left].toLowerCase != s[right].toLowerCase){
+            return false;
+         }
+        }
+        return true;
     }
 }
