a06d1614d848c7151e2532531a43de2d49e4cdc1
Sai Teja <98050865+Saitejachatradi@users.noreply.github.com>
2026-05-03 13:20:03 -0500

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       int[] a = new int[26];
+       if(s.length() != t.length()){
+        return false;
+       }
+for(int i = 0 ; i < s.length() ; i++){
+        a[s.charAt(i) - 'a']++;
+        a[t.charAt(i) - 'a']--;
+       }
+       for(int count: a){
+        if(count != 0){
+            return false;
+        }
+       }
+       return true;
+    }
+}
