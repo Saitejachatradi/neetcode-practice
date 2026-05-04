c5c8c2f4d58bd7cbec395ec1cbf196e9a9dac842
Sai Teja <98050865+Saitejachatradi@users.noreply.github.com>
2026-05-04 08:30:41 -0500

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for(int price: prices){
+            if(price < minPrice){
+                minPrice = price;
+            }
+            int profit = price - minPrice;
+            maxProfit = Math.max(profit,maxProfit);
+        }
+        return maxProfit;
+    }
+}
